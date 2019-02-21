d2d8456d243a396202abd5ebb0bd5cc27fa2f324
dmko1610 <trigger1610@gmail.com>
2019-02-21 16:43:07 +0400

    #jbzdf added popup window, some styles, icons

--- src/components/MainPageHeader.tsx
+++ src/components/MainPageHeader.tsx
@@ -1,20 +1,47 @@
 import * as React from 'react'
 import logo from '../images/organize.svg'
+import Form from '../routes/RegisterForm'
 import '../styles/MainPageHeader.less'
 
-class MainPageHeader extends React.Component {
+interface Props {
+  text: string
+}
+
+interface State {
+  showForm: boolean
+}
+
+class MainPageHeader extends React.Component<Props, State> {
+
+  constructor(props: Props) {
+    super(props)
+    this.state = {
+      showForm: true
+    }
+  }
+
+  public toggleForm() {
+    this.setState({
+      showForm: !this.state.showForm
+    })
+  }
+
   public render() {
-    return (      
-        <header className="main-page__header">
-          <img src={ logo } className="main-page__logo" alt="logo" />
-          <h2 className="main-page__title">Scheduler v.0.1</h2>
-          <button className="main-page__button" value="JOIN">JOIN</button>
-          <h1>PLAN.EXECUTE.DIVIDE</h1>
-          <h3>we've developed it. and have made our best. <br />
-            cuz' we are professionals</h3>
-        </header>      
+    return (
+      <header className="main-page__header">
+        <img src={logo} className="main-page__logo" alt="logo" />
+        <h2 className="main-page__title">Scheduler v.0.1</h2>
+        <button className="main-page__button" onClick={this.toggleForm.bind(this)} >JOIN</button>
+        <h1>PLAN.EXECUTE.DIVIDE</h1>
+        <h3>we've developed it. and have made our best. <br />
+          cuz' we are professionals</h3>
+        {
+          this.state.showForm ? <Form/> : null
+        }
+      </header>
     )
   }
+
 }
 
 export default MainPageHeader
---
+++ src/routes/RegisterForm.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react'
+import git from '../images/github-logo.svg'
+import google from '../images/google-plus.svg'
+import linkedin from '../images/linkedin.svg'
+import vk from '../images/vk-social-logotype.svg'
+import '../styles/RegisterForm.less'
+// import {
+//     BrowserRouter,
+//     Route,
+//     Switch
+// } from 'react-router-dom'
+
+// interface Props {
+//   text: string
+//   closePopup: boolean
+// }
+
+function Form() {
+  return (
+    <div className='register-form'>
+        <button className="sign-up">SIGN UP</button>
+        <button className="sign-in">SIGN IN</button>
+        <img src={vk} alt="vk" width="50px" height="50px"/>
+        <img src={git} alt="git" width="50px" height="50px"/>
+        <img src={linkedin} alt="linkedin" width="50px" height="50px"/>
+        <img src={google} alt="google" width="50px" height="50px"/>
+        {/* <button onClick={this.props.closePopup}>close me</button> */}
+    </div>
+  );
+
+}
+
+export default Form
